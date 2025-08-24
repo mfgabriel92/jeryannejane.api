73133ff260417670b204bac2d727f6d5483998f2
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-08-24 11:15:38 +0800

    feat: update CORS allowed origins to include Netlify deployment

--- src/main/java/me/jeryannejane/api/config/CORSConfig.java
+++ src/main/java/me/jeryannejane/api/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173/", "https://jeryannejane.me/")
+            .allowedOrigins("http://localhost:5173/", "https://jeryannejane.me/", "https://sparkly-twilight-26d006.netlify.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Content-Type")
             .maxAge(3600);
